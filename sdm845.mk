50b01518d778754f3d19c3f8b4166d5b2e2b5b28
Chris Crump <chris@cypheros.co>
2020-05-12 03:45:40 -0400

    sdm845-common: Remove judy permissions target

--- sdm845.mk
+++ sdm845.mk
@@ -286,9 +286,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/permissions/privapp-permissions-judy.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-judy.xml
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
